caaf9a3efc74ff2388d1f78b843631aacc25caa9
tiagopaes <tiagopaes37@gmail.com>
2019-09-16 21:59:14 -0300

    Create register form ui

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,6 +3,79 @@
     <v-row align="center" justify="center">
       <h1>Register</h1>
     </v-row>
-    <v-row align="center" justify="center"></v-row>
+    <v-row align="center" justify="center">
+      <v-card class="elevation-12">
+        <v-card-text>
+          <form @submit.prevent="handleSubmit" id="register-form">
+            <v-text-field
+              v-model="name"
+              required
+              label="Name"
+              name="name"
+              prepend-icon="person"
+              type="text"
+            ></v-text-field>
+            <v-text-field
+              v-model="email"
+              label="Email"
+              name="email"
+              prepend-icon="email"
+              type="email"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              label="Password"
+              name="password"
+              prepend-icon="lock"
+              type="password"
+              required
+            ></v-text-field>
+          </form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            type="submit"
+            form="register-form"
+            :loading="loading"
+            :disabled="disableSubmit"
+            block
+            color="primary"
+          >Register</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-row>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Register',
+  data() {
+    return {
+      name: null,
+      email: null,
+      password: null,
+      loading: false
+    };
+  },
+  computed: {
+    disableSubmit() {
+      if (
+        this.name &&
+        this.name.trim() != '' &&
+        (this.email && this.email.trim() != '') &&
+        (this.password && this.password.trim() != '')
+      ) {
+        return false;
+      }
+      return true;
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log("eae");
+    }
+  }
+};
+</script>
